327cbf9040f2a49d4d64e17cbb73afe9f85e643c
Gustavo Allfadir <gustavo.allfadir@gmail.com>
2021-01-25 21:40:32 -0600

    fibonacci

---
+++ fibonacci/fibonacci.dart
@@ -0,0 +1,31 @@
+class Fibonacci {
+  int max;
+
+  Fibonacci(max) {
+    this.max = max;
+  }
+
+  imprimir_secuencia() {
+    var a = 0;
+    var b = 1;
+    var c = a + b;
+    print("Inicializando secuencia fibonacci del 0 al ${this.max}:");
+    print(a);
+    print(b);
+    print(c);
+    while (c <= this.max) {
+      a = b;
+      b = c;
+      c = a + b;
+      if (c > this.max) {
+        break;
+      }
+      print(c);
+    }
+  }
+}
+
+void main() {
+  var fib = Fibonacci(300);
+  fib.imprimir_secuencia();
+}
